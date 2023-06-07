46d1c4d5671927daf85f533f68489543b0f67b34
Neth Botheju <nbot0003@student.monash.edu>
2023-06-08 00:34:22 +1000

    *NEW* Adding character cell to guild page

---
+++ Shroom/GuildTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  GuildTableViewCell.swift
+//  Shroom
+//
+//  Created by Neth Botheju on 8/6/2023.
+//
+
+import UIKit
+
+class GuildTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leaderBadge: UIImageView!
+    
+    @IBOutlet weak var userDisplayName: UILabel!
+    
+    @IBOutlet weak var userDisplayImage: UIImageView!
+    
+    @IBOutlet weak var userCharName: UILabel!
+    
+    @IBOutlet weak var userCharLevel: UILabel!
+    
+    @IBOutlet weak var healthBar: UIProgressView!
+    
+    @IBOutlet weak var expBar: UIProgressView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
